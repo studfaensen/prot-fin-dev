ce54450508d674c735b13942533775eb07a13a0a
Franz-Eric Sill <107982913+qwerdenkerXD@users.noreply.github.com>
2024-04-07 17:19:16 +0200

    improved plot_sequence_lengths.R to support multiple FASTA files to plot

--- methods/plot_sequence_lengths.R
+++ methods/plot_sequence_lengths.R
@@ -1,6 +1,6 @@
 args <- commandArgs(trailingOnly = TRUE)
 
-if (length(args) != 2) {
+if (length(args) < 2) {
     message("Usage: Rscript plot_sequence_lengths.R <fasta-file> <out-file.png>")
     q()
 }
@@ -9,15 +9,20 @@ if (length(args) != 2) {
 library(seqinr)
 library(ggplot2)
 library(ggdist)
+library(tibble)
 
 # Read the FASTA file
-fasta_file <- args[1]
-sequences <- read.fasta(file = fasta_file, seqtype = "AA", as.string = TRUE)
+fasta_files <- args[-length(args)]
+sequence_lengths <- lapply(fasta_files, function(f) {
+  sequences <- read.fasta(file = f, seqtype = "AA", as.string = TRUE, seqonly=TRUE)
+  sapply(sequences, nchar)
+})
 
-# Extract sequence lengths
-sequence_lengths <- sapply(sequences, nchar)
+groups <- lapply(seq_along(sequence_lengths), function(i) {tibble(value = sequence_lengths[[i]], group = fasta_files[i])})
 
-plt <- ggplot(data.frame(length = sequence_lengths), aes(x = "", y = length)) +
+data <- dplyr::bind_rows(groups)
+
+plt <- ggplot(data, aes(x = group, y = value)) +
   stat_halfeye(fill = "darkgray", width = 2) +
   geom_boxplot(width = 0, outlier.colour = "black", outlier.size = 0.1) +
   coord_flip() +
@@ -26,4 +31,4 @@ plt <- ggplot(data.frame(length = sequence_lengths), aes(x = "", y = length)) +
   labs(x = "Frequencies", y = "Lengths") +
   ggtitle("Raincloud Plot of Sequence Lengths")
 
-ggsave(args[2], plot = plt, width = 8, height = 3, dpi = 300)
+ggsave(args[length(args)], plot = plt, width = 8, height = 3, dpi = 300)
